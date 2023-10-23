f2a482b5e426fa6aff2ac316fd51f7852710d0a5
Ze Jin <zj123@ic.ac.uk>
2023-10-23 17:06:19 +0100

    test

--- api/app.py
+++ api/app.py
@@ -15,16 +15,10 @@ def submit():
 
 
 @app.route("/query", methods=["GET"])
-def query():
+def process_query():
     input_query = request.args.get("q")
-    process_query(input_query)
 
-
-def process_query(input_query):
-
-    if input_query == "dinosaurs":
-        return "Dinosaurs ruled the Earth 200 million years ago"
-    elif input_query == "asteroids":
-        return "Unknown"
+    if input_query:
+        return f"Q = {input_query}"
     else:
-        return "Error"
+        return "Nothing"
