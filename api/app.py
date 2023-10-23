e95a3c6527824e44fb0897c25de7720162935121
Ze Jin <zj123@ic.ac.uk>
2023-10-23 16:47:24 +0100

    Process_query added

--- api/app.py
+++ api/app.py
@@ -12,3 +12,13 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+@app.route("/query", methods=["GET"])
+def process_query():
+    input_query = request.args.get("q")
+    
+    if input_query:
+        return f"Q = {input_query}"
+    else:
+        return f"Nothing"
