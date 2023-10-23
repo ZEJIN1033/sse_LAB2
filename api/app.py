ebab830e9ff09305fe9ba9193af39c6b7797063b
Ze Jin <zejin@zedemacbook-pro.mynet>
2023-10-23 23:05:43 +0100

    Add gender selection

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     input_gender = request.form.get("gender")
-    return render_template("hello.html", name=input_name, age=input_age, 
+    return render_template("hello.html", name=input_name, age=input_age,
                            gender=input_gender)
 
 
