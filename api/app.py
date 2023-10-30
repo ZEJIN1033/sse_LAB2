ee37e76bcfd2640b54585ce71e28f51478115270
Ze Jin <zejin@dyn3189-220.wlan.ic.ac.uk>
2023-10-30 16:30:05 +0000

    test

--- api/app.py
+++ api/app.py
@@ -24,15 +24,17 @@ def query():
     return process_query(input_query)
 
 
-def process_query(input_query):
-    match = re.match(r'What is (\d+) plus (\d+)\?', input_query)
+def process_query(query):
+    # match = re.match(r'What is (\d+) plus (\d+)\?', input_query)
 
-    if match:
-        num1 = int(match.group(1))
-        num2 = int(match.group(2))
-        result = num1 + num2
+    # if match:
+    #     num1 = int(match.group(1))
+    #     num2 = int(match.group(2))
+    #     result = num1 + num2
+    #     return result
+    if query.startswith("Which of the following numbers is the largest"):
+        result = find_largest_number(query)
         return result
-
 #     question, numbers = input_query.split(':')
 #     if question.strip() == "Which of the following numbers is the largest":
 #         return render_template("result.html", result=find_largest(numbers))
@@ -48,3 +50,17 @@ def process_query(input_query):
 #         return largest
 #     else:
 #         return "No numbers provided"
+
+def find_largest_number(query):
+    # 使用正则表达式匹配数字部分
+    match = re.search(r'(\d+),\s*(\d+),\s*(\d+)', query)
+
+    if match:
+        A = int(match.group(1))
+        B = int(match.group(2))
+        C = int(match.group(3))
+        
+        largest = max(A, B, C)
+        return largest
+    else:
+        return "Invalid input"
