a65fa82a492be47033b81c8af7ce055b8a659f6f
Ze Jin <zejin@dyn3189-220.wlan.ic.ac.uk>
2023-10-30 18:07:48 +0000

    test

--- api/app.py
+++ api/app.py
@@ -14,8 +14,8 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     input_gender = request.form.get("gender")
-    return render_template("hello.html", name=input_name, \
-age=input_age, gender=input_gender)
+    return render_template("hello.html", name=input_name, age=input_age, 
+                           gender=input_gender)
 
 
 @app.route("/query", methods=["GET"])
